275030bc6cf4733031e56262c37198960fe0377d
Mohamed Elsayed <mo7amed0518@gmail.com>
2026-07-25 11:07:33 +0300

    feat: add company collections

--- EmployeeManagementSystem/Services/Company.cs
+++ EmployeeManagementSystem/Services/Company.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace EmployeeManagementSystem.Services
 {
     public class Company
     {
+        private readonly List<Employee> employees = new List<Employee>();
+        
+        private readonly Dictionary<int,Department> departments = new Dictionary<int,Department>();
+
+        private readonly Stack<string> actionHistory = new Stack<string>();
+        
+        private readonly Queue<Employee> onboardingQueue = new Queue<Employee>();
+        
+        private readonly HashSet<string> skills = new HashSet<string>();
+
     }
 }
